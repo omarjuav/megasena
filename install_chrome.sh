a3da0a1f3c3d6ef48a91244e1e31a77be857db2e
Omar Junio Antunes Vieria <omarjuav@yahoo.com.br>
2025-01-07 16:37:46 -0300

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -4,7 +4,6 @@ set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
 
-# Baixar o Google Chrome, se ainda não estiver instalado
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
@@ -12,11 +11,13 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
   rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/project/src # Certifique-se de retornar ao diretório original
+  cd $HOME/project/src # Make sure we return to where we were
 else
   echo "...Using Chrome from cache"
 fi
 
-# Adiciona o Chrome ao PATH para garantir que o Selenium consiga encontrá-lo
-export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
+# be sure to add Chromes location to the PATH as part of your Start Command
+# export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
+
+# add your own build commands...
 
